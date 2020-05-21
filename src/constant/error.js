4d9d8c7a4ed9ed68dbf8507e8590e4ad54d09623
cbhakar <chandrabhakar@gmail.com>
2020-05-21 21:53:24 +0530

    error msg changed

--- src/constant/error.js
+++ src/constant/error.js
@@ -2,12 +2,12 @@ const errorMessages = {
   fileSizeError: size => `File should be less than ${size} MB in size`,
   resolevError: "Please resolve below error",
   emailValidation: "Entered Email is not valid",
-  blank: field => `${field} cannot be blank`,
-  numberNotValid: field => `${field} is not valid.`,
-  priceNotValid: field => `${field} cannot be less than 0.`,
+  blank: field => `${field} can not be blank`,
+  numberNotValid: field => `${field} can be numeric only.`,
+  priceNotValid: field => `${field} can not be blank.`,
   maxLengthReached: (elementName, charLength) =>
-    `${elementName} should not be greater than ${charLength}.`,
-  resolve: 'Please below errors'
+    `${elementName} length should not be greater than ${charLength}.`,
+  resolve: 'Please resolve below error(s)'
 };
 
 export default errorMessages;
