64254f6ce8a867a1fbab2f510ebd21ea57afbdd4
Chandra Prakash Bhakar <chandrabhakar@gmail.com>
2020-05-21 19:07:31 +0530

    Update endpoint.js

--- src/services/endpoint.js
+++ src/services/endpoint.js
@@ -2,6 +2,6 @@ export default {
   listTea: () => 'https://cpdemo1.herokuapp.com/items',
   addTea: () => 'https://cpdemo1.herokuapp.com/items',
   imageUpload: () => 'https://cpdemo1.herokuapp.com/upload-image',
-  getTea: id => 'https://cpdemo1.herokuapp.com/item/${id}',
-  deleteTea: id => 'https://cpdemo1.herokuapp.com/item/${id}',
+  getTea: id => `https://cpdemo1.herokuapp.com/item/${id}`,
+  deleteTea: id => `https://cpdemo1.herokuapp.com/item/${id}`,
 }
